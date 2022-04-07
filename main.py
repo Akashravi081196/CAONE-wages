72bd2c2ee73d0a19353b3d0f20226c7605c2b5b8
Akashravi081196 <102610264+Akashravi081196@users.noreply.github.com>
2022-04-07 16:40:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -26,18 +26,67 @@ class Employee:
         self.__TaxCredit = TaxCredit
         self.__StandardBand = StandardBand
 
-
-#Create a dictionary and giving value so that we can use in feature for the wage calculation and for the test
     def computePayment(self, HoursWorked, date):
-        dict = {
-                "name": self.__FirstName + " " + self.__LastName,
-                "Regular Hours Worked": self.__RegHours,
-                "Regular Rate": self.__HourlyRate,
-                "Standard Rate Pay": self.__StandardBand,
-                "Tax Credit": self.__TaxCredit,
-                }
-        print(dict)
+        # Over time worked calculations:
+        #if (HoursWorked < 0):
+            #raise ValueError('Hours Worked cannot be negative!!!')
+        #else:
+        OverTimeWorked = HoursWorked - self.__RegHours
+        print(OverTimeWorked)
+
+        Over_Time_Rate = self.__HourlyRate * self.__OTMultiple
+        print(Over_Time_Rate)
+
+        Regular_Pay = self.__RegHours * self.__HourlyRate
+        print(Regular_Pay)
+
+        Over_Time_pay = Over_Time_Rate * OverTimeWorked
+        print(Over_Time_pay)
+
+        Gross_Pay = Regular_Pay + Over_Time_pay
+        print(Gross_Pay)
+
+        Higher_Rate_Pay = Gross_Pay - self.__StandardBand
+        print(Higher_Rate_Pay)
+
+        # 20% Standard Tax
+        Std_Tax = Gross_Pay * 0.2
+        rnd_Std_Tax = round(Std_Tax)
+        print(rnd_Std_Tax)
+
+        # 40% of Higher rate Pay
+        # Higher_Tax = Higher_Rate_Pay*0.4
+        # if (Higher_Rate_Pay < 0):
+        # raise ValueError("Higher Tax cannot be negative")
+        # else:
+        Higher_Tax = Higher_Rate_Pay * 0.4
+        print(Higher_Tax)
+
+        # if (Higher_Tax < 0):
+        # raise ValueError("Net Pay cannot be negative.")
+
+        Total_Tax = rnd_Std_Tax + Higher_Tax
+        print(Total_Tax)
+
+        Net_Tax = Total_Tax - self.__TaxCredit
+        print(Net_Tax)
+
+        # PRSI (at 4%)
+        PRSI = Gross_Pay * 0.04
+        print(PRSI)
+
+        Net_Deduction = Net_Tax + PRSI
+        print(Net_Deduction)
+
+        Net_Pay = Gross_Pay - Net_Deduction
+        print(Net_Pay)
+
+        #if (self.__RegHours > HoursWorked):
+            #raise ValueError("Regular Hours Worked cannot exceed hours worked")
+
+
+
 
 e = Employee(12345, 'Green', 'Joe', 37, 16, 1.5, 72, 710)
-e.computePayment(42,'31/10/2022')
+e.computePayment(42, '31/10/2022')
 
